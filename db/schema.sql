fd1989f0f9ccd28682861090661d261c797eb0a8
Thannia Hernandez <thaherna1@gmail.com>
2024-06-20 11:22:01 -0500

    added DB for employee tracker and created tables in SQL file

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IS EXISTS employee;
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+);
+
+CREATE TABLE employee(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER NULL
+)
