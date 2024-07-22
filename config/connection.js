234cbce27747993f194df290249ae2c4ce56b74f
Thannia Hernandez <thaherna1@gmail.com>
2024-07-22 16:07:53 -0500

    debugged

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'employee_db',
     password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    port: 5432,
 });
 
 module.exports = pool;
